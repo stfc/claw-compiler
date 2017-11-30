1c1e29a53bef97895822d84cce5349e87a8fb636
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-11-30 10:42:32 +0000

    #1 Update driver script to generate a new Fortran file rather than overwrite input

--- driver/clawfc.py
+++ driver/clawfc.py
@@ -26,20 +26,25 @@ def claw_driver(argv):
                                                         script_file)
     # Use the OMNI frontend to generate the XcodeML representation of
     # the Fortran file
-    (dir_path, xml_file) = path.split(fortran_file)
-    if xml_file.endswith(".F90"):
-        xml_file = xml_file.replace(".F90", ".xml", 1)
-    elif xml_file.endswith(".f90"):
-        xml_file = xml_file.replace(".f90", ".xml", 1)
+    (dir_path, input_fortran_file) = path.split(fortran_file)
+    if input_fortran_file.endswith(".F90"):
+        xml_file = input_fortran_file.replace(".F90", ".xml", 1)
+        output_fortran_file = input_fortran_file.replace(".F90", ".new.F90", 1)
+    elif input_fortran_file.endswith(".f90"):
+        xml_file = input_fortran_file.replace(".f90", ".xml", 1)
+        output_fortran_file = input_fortran_file.replace(".f90", ".new.f90", 1)
     else:
         raise Exception("Fortran file must have .f90 or .F90 suffix but "
                         "got {0}".format(xml_file))
+
     xml_file = path.join(dir_path, xml_file)
+    output_fortran_file = path.join(dir_path, output_fortran_file)
+
     call(["F_Front", fortran_file, "-o", str(xml_file)])
     print "Produced XCodeML file: {0}".format(xml_file)
 
-    # Then transform this XcodeML representation using CLAW and de-compile
-    # it back to Fortran
+    # Then transform this XcodeML representation using CLAW and use OMNI to
+    # de-compile it back to Fortran
     call(["/usr/bin/java", "-Xmx200m", "-Xms200m", "-cp", CLASS_PATH,
           "claw.ClawX2T", "--config-path={0}".format(CLAW_CONFIG_FILE),
           "--schema=/home/kbc59144/MyInstalls/etc/claw_config.xsd",
@@ -47,7 +52,7 @@ def claw_driver(argv):
           "-M/home/kbc59144/Projects/code_fragments",
           "-M/home/kbc59144/MyInstalls/fincludes",
           "-o", "claw_5f_example_f90_out.xml",
-          "-f", fortran_file,
+          "-f", output_fortran_file,
           "-script", script_file,
           xml_file])
 
